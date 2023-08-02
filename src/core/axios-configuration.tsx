b8822a7d6f4a4f20bc23a3382ad8a7afb2563cff
mohamedfahii <mohamed.fathii@hotmail.com>
2023-08-02 16:21:39 +0300

    finishlist

--- src/core/axios-configuration.tsx
+++ src/core/axios-configuration.tsx
@@ -5,7 +5,7 @@ export const customAxios = axios.create({
   headers: {
     "Content-type": "application/vnd.github+json",
     Accept: "application/vnd.github+json",
-    Authorization: `Bearer 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`,
+    Authorization: `Bearer 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`,
     "X-GitHub-Api-Version": "2022-11-28",
   },
 });
